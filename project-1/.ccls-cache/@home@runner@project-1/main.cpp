84150ebadeb29da571a1d7d52e1ad53a288045cc
EkeneObiadi <105914334+EkeneObiadi@users.noreply.github.com>
2022-05-20 19:55:08 +1000

    project
    updated file

---
+++ project-1/.ccls-cache/@home@runner@project-1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+std::string username = "newton";
+std::string lastname = "ezike";
+int userage = 23;
+bool istall = "false";
+int main() {
+
+  std::cout << "Hello World!\n";
+  std::cout <<" How are you all doing \n ";
+  std::cout << "my name is "<< username << " and i am " << userage << " years old \n";
+   std::cout << "What is Your last Name "<< username << ". \n";
+  std::cout << "My last Name is "<< lastname << ". \n";
+   if(istall){
+    std:: cout << "you are tall \n";
+     if else() {
+       "you are not tall \n";
+       }
+  }   
+  }
